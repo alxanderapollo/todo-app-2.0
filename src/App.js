9fbf27ab574aef540c1a91e1ae2a89d73e445ccd
Alex <alexsalazarny@gmail.com>
2022-02-15 07:57:01 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "../src/components/Card";
 import Header from "./components/Header";
 function App() {
   return (
     <>
       <Header />
-      <div className="container"></div>
+      <div className="container">
+        <Card />
+      </div>
     </>
   );
 }
